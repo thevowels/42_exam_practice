4e7403558daff567e2d8c310596052a23e9862a2
Aung Phyo Htet <aungphyohtet1@gmail.com>
2026-02-12 11:05:43 +0700

    step by step fix on broken_gnl

---
+++ Rank03/broken_gnl/broken_gnl_step1.c
@@ -0,0 +1,170 @@
+/*
+	Step 1: Here I will fix the syntax errors first.
+		Most of them are syntatx error but some of them are related to the nature of the problem. which means logical errors are also inevitable if we encounter.
+		You can see most of the error instantly just by compiling cc broken_gnl.c.
+		The compiler shows us all errors it can see at the compile process. If you want to go a little bit deeper, you can make some research about compile languages.
+
+	Note: cc broken_gnl_before_fix.c &> out
+		  cc broken_gnl_after_fix.c &> outfix
+	// This will help you to compare the results before and after fix.
+*/
+
+/*
+broken_gnl_step1.c:7:10: error: 'broken_gnl.h' file not found with <angled> include; use "quotes" instead
+#include <broken_gnl.h>
+         ^~~~~~~~~~~~~~
+         "broken_gnl.h"
+
+		 // Here we can see we are using <> notation instead of "". It even shows us the solution right away.
+		
+		 Fix: use "" notation instead of <>
+		 fix and compile again.
+*/
+#include "broken_gnl.h"
+
+/*
+broken_gnl_step1.c:28:10: error: incompatible pointer to integer conversion returning 'char *' from a function with result type 'char'; dereference with * [-Wint-conversion]
+                return (s + i);
+
+				// here we've got error as we are returning char * instead of char.
+				Here, we need to fix in 2 ways.
+					1. change return type of function
+					2. change return value of the function.
+				What to choose? if you examine about the nature of the usage of the function in gnl , we can see that we want to get the location of the char. so the return type should be char *
+				There is no absolute way to tell we are correct or not as we are fixing other person's code. Lets stick with case 1 for now.
+				If there is an error, we can always fix the code. Right?
+				
+				Current Fix:  change return type of function
+
+*/
+char	*ft_strchr(char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] && s[i] != c)
+		i++;
+	if (s[i] == c)
+		return (s + i);
+	else
+		return (NULL);
+}
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		((char *)dest)[i] = ((char *)src)[i];
+		i++;
+	}
+	return (dest);
+}
+
+size_t	ft_strlen(char *s)
+{
+	size_t	ret;
+
+	ret = 0;
+	while (*s)
+	{
+		s++;
+		ret++;
+	}
+	return (ret);
+}
+
+int	str_append_mem(char **s1, char *s2, size_t size2)
+{
+	size_t	size1;
+	char	*tmp;
+
+	size1 = s1 ? ft_strlen(s1) : 0;
+	tmp = malloc(size2 + size1 + 1);
+	if (!tmp)
+		return (0);
+	if (*s1)
+		ft_memcpy(tmp, *s1, size1);
+	ft_memcpy(tmp + size1, s2, size2);
+	tmp[size1 + size2] = 0;
+	free(*s1);
+	*s1 = tmp;
+	return (1);
+}
+
+int	str_append_str(char **s1, char s2)
+{
+	return (str_append_mem(s1, s2, ft_strlen(s2)));
+}
+
+void	ft_memmove(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+
+	if (dest < src)
+		return (ft_memcpy(dest, src, n));
+	else if (dest == src)
+		return (dest);
+	i = n;
+	while (i > 0)
+	{
+		i--;
+		((char *)dest)[i] = ((char)src)[i];
+	}
+	return (dest);
+}
+
+char	get_next_line(int fd)
+{
+	static char b[BUFFER_SIZE + 1] = ();
+	char *ret;
+	int read_ret;
+
+	ret = NULL;
+	char *tmp = ft_strchr(b, '\n');
+	while (!tmp)
+	{
+		if (!str_append_str(&ret, b))
+			return (NULL);
+		b[0] = '\0';
+		read_ret = read(fd, b, BUFFER_SIZE);
+		if (read_ret == -1)
+			return (NULL);
+		if (read_ret == 0)
+			break ;
+		b[read_ret] = 0;
+		tmp = ft_strchr(b, '\n');
+	}
+	if (tmp)
+	{
+		if (!str_append_mem(&ret, b, tmp - b + 1))
+		{
+			free(ret);
+			return (NULL);
+		}
+		ft_memmove(b, tmp + 1, ft_strlen(tmp + 1) + 1);
+	}
+	else
+	{
+		b[0] = '\0';
+		if (!ret || !*ret)
+		{
+			free(ret);
+			return (NULL);
+		}
+	}
+	return (ret);
+}
+
+#include <stdio.h>
+#include <fcntl.h>
+
+int main(void)
+{
+	int fd= open("broken_gnl.c", O_RDONLY);
+
+	char *str = get_next_line(fd);
+	printf("%s\n", str);
+}
